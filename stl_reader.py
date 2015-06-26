f40d0a5b9697f1fcc847909b9c9994f712f32b86
Sergiy Tsymbal <tcymbal@gmail.com>
2015-06-26 23:17:38 +0300

    ready reader

--- stl_reader.py
+++ stl_reader.py
@@ -1,11 +1,58 @@
-class AsciiFormatError(Exception):
+class StlAsciiFormatError(Exception):
     pass
 
 def read_stl_ascii(path):
     with open(path) as f:
+        counter = 0
         line = f.readline()
-        if line.startswith('solid'):
-            pass
-        else:
-            raise AsciiFormatError
-    return True
+        if not line.startswith('solid'):
+            raise StlAsciiFormatError(counter, line)
+
+        result = []
+        while True:
+            line = f.readline()[:-1]
+            counter+=1
+            if line == 'endsolid':
+                return result
+            elif line.startswith('facet normal '):
+                n1, n2, n3 = line[13:].split()
+
+            line = f.readline()[:-1]
+            counter+=1
+            if not line == 'outer loop':
+                raise StlAsciiFormatError(counter, line)
+
+            line = f.readline()[:-1]
+            counter+=1
+            if line.startswith('vertex '):
+                v1, v2, v3 = line[7:].split(' ')
+            else:
+                raise StlAsciiFormatError(counter, line)
+
+            line = f.readline()[:-1]
+            counter+=1
+            if line.startswith('vertex '):
+                v4, v5, v6 = line[7:].split(' ')
+            else:
+                raise StlAsciiFormatError(counter, line)
+
+            line = f.readline()[:-1]
+            counter+=1
+            if line.startswith('vertex '):
+                v7, v8, v9 = line[7:].split(' ')
+            else:
+                raise StlAsciiFormatError(counter, line)
+
+            line = f.readline()[:-1]
+            counter+=1
+            if not line == 'endloop':
+                raise StlAsciiFormatError(counter, line)
+
+            line = f.readline()[:-1]
+            counter+=1
+            if not line == 'endfacet':
+                raise StlAsciiFormatError(counter, line)
+
+            result.append([n1, n2, n3, v1, v2, v3, v4, v5, v6, v7, v8, v9])
+
+    return result
--- test/test_stl.py
+++ test/test_stl.py
@@ -1,4 +1,4 @@
-from stl_reader import read_stl_ascii, AsciiFormatError
+from stl_reader import read_stl_ascii, StlAsciiFormatError
 import unittest
 
 class TestSTL(unittest.TestCase):
@@ -6,4 +6,8 @@ class TestSTL(unittest.TestCase):
         read_stl_ascii('test/data/cone_and_sphere.stl')
 
     def test_empty(self):
-        self.assertRaises(AsciiFormatError, read_stl_ascii, 'test/data/empty.stl')
+        self.assertRaises(StlAsciiFormatError, read_stl_ascii, 'test/data/empty.stl')
+
+    def test_wrong_start(self):
+        self.assertRaises(StlAsciiFormatError, read_stl_ascii, 'test/data/wrong_start.stl')
+    
