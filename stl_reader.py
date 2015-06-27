08cf0b32b8929ee17075e7ef58efcc1f5d8fedb8
Sergiy Tsymbal <tcymbal@gmail.com>
2015-06-27 11:39:47 +0300

    invented classes

--- stl_reader.py
+++ stl_reader.py
@@ -1,10 +1,24 @@
+class Triangle(object):
+    def __init__(self, normal, point1, point2, point3):
+        self.normal = normal
+        self.point1 = point1
+        self.point2 = point2
+        self.point3 = point3
+
+class Stl(object):
+    def __init__(self, triangles):
+        self.triangles = triangles
+
+    def __len__(self):
+        return len(self.triangles)
+
 class StlAsciiFormatError(Exception):
     pass
 
-class Stl(object):
-    def __init__(self, path):
+class StlReader(object):
+    def __init__(self):
         self.counter = -1
-        self.data = self.read_ascii(path)
+        # self.data = self.read_ascii(path).triangles
 
     def read_line_equals(self, f, line):
         self.line = f.readline()[:-1]
@@ -19,10 +33,10 @@ class Stl(object):
             v1, v2, v3 = self.line[shift:].split(' ')
         else:
             raise StlAsciiFormatError(self.counter, self.line)
-        return float(v1), float(v2), float(v3)
+        return [float(p) for p in v1, v2, v3]
 
     def read_ascii(self, path):
-        result = []
+        triangles = []
         with open(path) as f:
             self.read_line_equals(f, 'solid')
 
@@ -30,19 +44,21 @@ class Stl(object):
                 self.line = f.readline()[:-1]
                 self.counter+=1
                 if self.line == 'endsolid':
-                    return result
+                    stl = Stl(triangles)
+                    return stl
                 elif self.line.startswith('facet normal '):
-                    n1, n2, n3 = self.line[13:].split()
+                    normal = [float(p) for p in self.line[13:].split()]
 
                 self.read_line_equals(f, 'outer loop')
 
-                v1, v2, v3 = self.read_3_floats(f, 'vertex ', 7)
-                v4, v5, v6 = self.read_3_floats(f, 'vertex ', 7)
-                v7, v8, v9 = self.read_3_floats(f, 'vertex ', 7)
+                point1 = self.read_3_floats(f, 'vertex ', 7)
+                point2 = self.read_3_floats(f, 'vertex ', 7)
+                point3 = self.read_3_floats(f, 'vertex ', 7)
 
                 self.read_line_equals(f, 'endloop')
                 self.read_line_equals(f, 'endfacet')
- 
-                result.append([float(n1), float(n2), float(n3), v1, v2, v3, v4, v5, v6, v7, v8, v9])
+                
+                triangle = Triangle(normal, point1, point2, point3)
+                triangles.append(triangle)
 
-        return result
+        raise StlAsciiFormatError(self.counter, self.line)
--- test/test_stl.py
+++ test/test_stl.py
@@ -1,22 +1,30 @@
-from stl_reader import Stl, StlAsciiFormatError
+from stl_reader import StlReader, StlAsciiFormatError
 import unittest
 
 class TestSTL(unittest.TestCase):
+    def setUp(self):
+        self.reader = StlReader()
+        self.read_ascii = self.reader.read_ascii
+
     def test_read(self):
-        stl = Stl('test/data/cone_and_sphere.stl')
-        self.assertTrue(len(stl.data))
+        stl = self.read_ascii('test/data/cone_and_sphere.stl')
+        self.assertEquals(len(stl), 2430)
 
     def test_empty(self):
-        self.assertRaises(StlAsciiFormatError, Stl, 'test/data/empty.stl')
+        self.assertRaises(StlAsciiFormatError, self.read_ascii, 'test/data/empty.stl')
 
     def test_wrong_start(self):
-        self.assertRaises(StlAsciiFormatError, Stl, 'test/data/wrong_start.stl')
+        self.assertRaises(StlAsciiFormatError, self.read_ascii, 'test/data/wrong_start.stl')
 
     def test_one_triangle(self):
-        stl = Stl('test/data/one_triangle.stl')
-        self.assertEquals(len(stl.data), 1)
+        stl = self.read_ascii('test/data/one_triangle.stl')
+        self.assertEquals(len(stl), 1)
+        triangle = stl.triangles[0]
         exp_data = [0.062913, -0.004007, -0.998011, 
                     0.62160000, -0.07850000, -4.96050000,
                     0.00000000, 0.00000000, -5.00000000,
                     0.62660000, 0.00000000, -4.96050000]
-        self.assertEquals(stl.data[0], exp_data)
+        self.assertEquals(triangle.normal, exp_data[0:3])
+        self.assertEquals(triangle.point1, exp_data[3:6])
+        self.assertEquals(triangle.point2, exp_data[6:9])
+        self.assertEquals(triangle.point3, exp_data[9:12])
