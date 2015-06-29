511b4b1fa2c9a20ffc01a39a4e279b141b4a070b
Sergiy Tsymbal <tcymbal@gmail.com>
2015-06-30 00:49:14 +0300

    class point, bounding box

--- stl_reader.py
+++ stl_reader.py
@@ -3,41 +3,161 @@ class Geometry(object):
         self.eps = 0.00000001
         self.eps2 = self.eps**2
 
-    def point_distance2(self, p1, p2):
-        result = sum((p1[i]-p2[i])**2 for i in range(3))
-        return result
+class Point(Geometry):
+    def __init__(self, coords):
+        super(Point, self).__init__()
+        self.coords = coords
+        self.points = [self]
+        self.x = coords[0]
+        self.y = coords[1]
+        self.z = coords[2]
+        if len(coords) != 3:
+            raise TypeError
+        self.bounding_box = self.get_bounding_box()
+
+    def __len__(self):
+        return len(self.coords)
+
+    def __repr__(self):
+        return 'Point ({}, {}, {})'.format(self[0], self[1], self[2])
+
+    def __iter__(self):
+        return iter(self.coords)
+
+    def __getitem__(self, index):
+        return self.coords[index]
+
+    def __eq__(self, point2):
+        return all(abs(self[i]-point2[i]) < self.eps for i in range(3))
+
+    def get_bounding_box(self):
+        return BoundingBox(self, self)
+
+class BoundingBox(Geometry):
+    def __init__(self, point_min, point_max):
+        super(BoundingBox, self).__init__()
+        if type(point_min) != Point:
+            raise TypeError
+        if type(point_max) != Point:
+            raise TypeError
+        self.min = point_min
+        self.max = point_max
+        self.points = [self.min, self.max]
+
+    def __repr__(self):
+        return 'Bounding box ({}, {}, {}), ({}, {}, {})'.format(
+            self.min[0], self.min[1], self.min[2], self.max[0], self.max[1], self.max[2])
+
+    def __getitem__(self, index):
+        return self.points[index]
+
+    def __contains__(self, point):
+        return all(self.min[i] <= point[i] <= self.max[i] for i in range(3))
+
+    def __add__(self, bounding_box):
+        # p_min = Point(min(self.min[i], bounding_box.min[i]) for i in range(3))
+        p_min = Point([
+            min(self.min.x, bounding_box.min.x),
+            min(self.min.y, bounding_box.min.y),
+            min(self.min.z, bounding_box.min.z)])
+        # p_max = Point(max(self.max[i], bounding_box.max[i]) for i in range(3))
+        p_max = Point([
+            max(self.max.x, bounding_box.max.x),
+            max(self.max.y, bounding_box.max.y),
+            max(self.max.z, bounding_box.max.z)])
+        return BoundingBox(p_min, p_max)
 
 class Triangle(Geometry):
     def __init__(self, normal, point1, point2, point3):
         super(Triangle, self).__init__()
-        self.normal = normal
-        self.points = [point1, point2, point3]
+        self.normal = Point(normal)
+        self.point1 = Point(point1)
+        self.point2 = Point(point2)
+        self.point3 = Point(point3)
+        self.points = [self.point1, self.point2, self.point3]
+        self.bounding_box = self.get_bounding_box()
+
+    def __repr__(self):
+        return 'Triangle ' + ','.join('(' + ','.join(str(c) for c in p.coords) + ')' for p in self.points)
+
+    def __iter__(self):
+        return iter(self.points)
+
+    def __getitem__(self, index):
+        return self.points[index]
 
     def contacts(self, other_triangle):
-        same_point_pairs = sum(self.point_distance2(p1, p2) < self.eps2
-            for p2 in other_triangle.points for p1 in self.points)
+        same_point_pairs = sum(p1 == p2 for p2 in other_triangle for p1 in self)
         return same_point_pairs >= 2
+
+    def point_distance2(self, point2):
+        result = sum((self[i]-point2[i])**2 for i in range(3))
+        return result
+
+    def get_bounding_box(self):
+        return self.point1.bounding_box + self.point2.bounding_box + self.point3.bounding_box
 
 class Stl(Geometry):
     def __init__(self, triangles):
         super(Stl, self).__init__()
         self.triangles = triangles
+        self.bounding_box = self.get_bounding_box()
 
     def __len__(self):
         return len(self.triangles)
 
+    def __iter__(self):
+        return iter(self.triangles)
+
+    def __getitem__(self, index):
+        return self.triangles[index]
+
+    def get_bounding_box(self):
+        inf = float('inf')
+        self.default_bounding_box = BoundingBox(
+            Point([inf,inf,inf]),
+            Point([-inf,-inf,-inf]))
+        result = self.default_bounding_box
+        for triangle in self:
+            result += triangle.bounding_box
+        return result
+
+    def add_triangle(self, triangle):
+        self.triangles.append(triangle)
+        self.bounding_box += triangle.bounding_box
+
+    def add_stl(self, other_stl):
+        for triangle in other_stl:
+            self.add_triangle(triangle)
+
     def contacts_triangle(self, other_triangle):
-        return any(my_triangle.contacts(other_triangle) 
-            for my_triangle in self.triangles)
+        if sum(other_triangle.points[i] in self.bounding_box for i in range(3)) > 1:
+            return any(my_triangle.contacts(other_triangle) 
+                for my_triangle in self)
+        else:
+            return False
     
     def contacts_stl(self, other_stl):
         return any(self.contacts_triangle(triangle) 
-            for triangle in other_stl.triangles)
+            for triangle in other_stl)
+
+    #TODO
+    def split_surfaces(self):
+        triangles_copy = self.triangles[:]
+        surfaces = []
+        for triangle in triangles_copy:
+            current_surface = Stl([triangle])
+            for other_triangle in triangles_copy:
+                if current_surface.contacts_triangle(other_triangle):
+                    current_surface.add_triangle(other_triangle)
+            surfaces.append(current_surface)
+            
+        return surfaces
 
 class StlAsciiFormatError(Exception):
     pass
 
-class StlReader(object):
+class StlReader(Stl):
     def __init__(self):
         self.counter = -1
 
@@ -83,3 +203,8 @@ class StlReader(object):
                 triangles.append(triangle)
 
         raise StlAsciiFormatError(self.counter, self.line)
+
+##p1 = Point([0,0,0])
+##p2 = Point([0,0,1])
+##p3 = Point([0,1,0])
+##t = Triangle([1,0,0],p1,p2,p3)
--- test/test_stl.py
+++ test/test_stl.py
@@ -3,6 +3,7 @@ import unittest
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
+        self.__name__ = 'SRL reader Test Case'
         reader = StlReader()
         self.read_ascii = reader.read_ascii
 
@@ -20,15 +21,15 @@ class TestStlReader(BaseTestCase):
     def test_one_triangle(self):
         stl = self.read_ascii('test/data/one_triangle.stl')
         self.assertEquals(len(stl), 1)
-        triangle = stl.triangles[0]
+        triangle = stl[0]
         exp_data = [0.062913, -0.004007, -0.998011, 
                     0.62160000, -0.07850000, -4.96050000,
                     0.00000000, 0.00000000, -5.00000000,
                     0.62660000, 0.00000000, -4.96050000]
-        self.assertEquals(triangle.normal, exp_data[0:3])
-        self.assertEquals(triangle.points[0], exp_data[3:6])
-        self.assertEquals(triangle.points[1], exp_data[6:9])
-        self.assertEquals(triangle.points[2], exp_data[9:12])
+        self.assertEquals(triangle.normal.coords, exp_data[0:3])
+        self.assertEquals(triangle[0].coords, exp_data[3:6])
+        self.assertEquals(triangle[1].coords, exp_data[6:9])
+        self.assertEquals(triangle[2].coords, exp_data[9:12])
 
     def test_non_existing(self):
         self.assertRaises(IOError, self.read_ascii, 'test/data/non_existing.stl')
@@ -36,27 +37,27 @@ class TestStlReader(BaseTestCase):
 class TestTriangle(BaseTestCase):
     def test_contacts(self):
         stl = self.read_ascii('test/data/contacting_triangles.stl')
-        triangle1 = stl.triangles[0]
-        triangle2 = stl.triangles[1]
+        triangle1 = stl[0]
+        triangle2 = stl[1]
         self.assertTrue(triangle1.contacts(triangle2))
 
     def test_does_not_contact(self):
         stl = self.read_ascii('test/data/separate_triangles.stl')
-        triangle1 = stl.triangles[0]
-        triangle2 = stl.triangles[1]
+        triangle1 = stl[0]
+        triangle2 = stl[1]
         self.assertFalse(triangle1.contacts(triangle2))
 
 class TestStl(BaseTestCase):
     def test_contacts_triangle(self):
         stl = self.read_ascii('test/data/separate_triangles.stl')
-        triangle1 = stl.triangles[0]
-        triangle2 = stl.triangles[1]
+        triangle1 = stl[0]
+        triangle2 = stl[1]
         self.assertTrue(stl.contacts_triangle(triangle1))
 
     def test_does_not_contact_triangle(self):
         stl = self.read_ascii('test/data/separate_triangles.stl')
-        stl1 = Stl([stl.triangles[0]])
-        self.assertFalse(stl1.contacts_triangle(stl.triangles[1]))
+        stl1 = Stl([stl[0]])
+        self.assertFalse(stl1.contacts_triangle(stl[1]))
 
     def test_contacts_stl(self):
         stl1 = self.read_ascii('test/data/separate_triangles.stl')
@@ -65,6 +66,12 @@ class TestStl(BaseTestCase):
 
     def test_does_not_contact_stl(self):
         stl = self.read_ascii('test/data/separate_triangles.stl')
-        stl1 = Stl([stl.triangles[0]])
-        stl2 = Stl([stl.triangles[1]])
+        stl1 = Stl([stl[0]])
+        stl2 = Stl([stl[1]])
         self.assertFalse(stl1.contacts_stl(stl2))
+
+    # @unittest.skip
+    def test_split_surfaces(self):
+        stl = self.read_ascii('test/data/cone_and_sphere.stl')
+        split_stls = stl.split_surfaces()
+        self.assertEquals(len(split_stls), 2)
